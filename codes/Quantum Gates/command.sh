ef82af7171c205b3ebe87cd8a9a61e15ec21e5ff
Anik Saha <aaniksahaa.2001@gmail.com>
2023-12-28 00:00:27 +0600

    a

---
+++ codes/Quantum Gates/command.sh
@@ -0,0 +1,2 @@
+jupyter nbconvert --to html hadamard_gate.ipynb
+jupyter nbconvert --to html pauli_gate.ipynb
--- codes/Quantum Gates/convert-all.sh
+++ codes/Quantum Gates/convert-all.sh
@@ -1,15 +1,7 @@
-Run in Linux
-
 jupyter nbconvert --to html hadamard_gate.ipynb
-
 jupyter nbconvert --to html pauli_gate.ipynb
-
 jupyter nbconvert --to html cnot_gate.ipynb
-
 jupyter nbconvert --to html toffoli_ccnot_gate.ipynb
-
 jupyter nbconvert --to html swap_gate.ipynb
-
 jupyter nbconvert --to html p_gate.ipynb
-
 jupyter nbconvert --to html t_gate.ipynb
